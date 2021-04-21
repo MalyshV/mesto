cd116466a388842ed674de6c810625d0dc60e072
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-04-21 13:19:23 +0300

    fix: index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
+let closeFormButton = document.querySelector('.input-container__button');
+let formElement = document.querySelector('.input-container');
+
+openPopupButton.addEventListener('click', togglePopup);
+closeFormButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
+popup.addEventListener('click', handleOverlayClick);
+formElement.addEventListener('submit', changeInfo);
 
 function togglePopup(event) {
   event.preventDefault();
   popup.classList.toggle('popup_is-opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
-
-closePopupButton.addEventListener('click', togglePopup);
 function handleOverlayClick(event) {
   if (event.target === event.currentTarget) {
     togglePopup(event);
   }
 }
-
-popup.addEventListener('click', handleOverlayClick);
-
-
-let formElement = document.querySelector('.input-container');
 
 document.querySelector('.input-container__button').onclick = changeInfo;
 
@@ -30,8 +30,4 @@ function changeInfo(event) {
 
   document.querySelector('.profile__user-name').textContent = name;
   document.querySelector('.profile__user-description').textContent = job;
-
-  togglePopup()
 }
-
-formElement.addEventListener('submit', changeInfo);
