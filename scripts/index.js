7d9df3db679ad9858b79d53b9480dd7d4045d7ae
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-05-01 21:12:11 +0300

    add new card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,28 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('[name="profile-form"]');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('#popup');
+const popup_card = document.querySelector('#popup_card');
+const closePopupButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('[name="profile-form"]');
+//const formElementCard = document.querySelector('[name="card-form"]');
+const openPopupCardButton = document.querySelector('.profile__add-button');
+const closePopupCardButton = document.querySelector('#button');
+const addNewCardButton = document.querySelector('.input-container__addbutton');
 let nameInput = document.querySelector('[name="user-name"]');
 let jobInput = document.querySelector('[name="user-job"]');
 let profileName = document.querySelector('.profile__user-name');
 let profileJob = document.querySelector('.profile__user-job');
+let placeInput = document.querySelector('[name="name"]');
+let imageInput = document.querySelector('[name="link"]');
+let placeName = document.querySelector('.element__title');
+let placeImage = document.querySelector('.element__image');
 
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
+closePopupCardButton.addEventListener('click', closePopupCard);
 formElement.addEventListener('submit', formSubmitHandler);
+openPopupCardButton.addEventListener('click', openPopupCard);
+
+
 
 function openPopup(event) {
   event.preventDefault();
@@ -24,6 +37,11 @@ function closePopup(event) {
   popup.classList.remove('popup_is-opened');
 }
 
+function closeAddPopup(event) {
+  event.preventDefault();
+  popup.classList.remove('popup_is-opened');
+}
+
 function formSubmitHandler(event) {
   event.preventDefault();
 
@@ -32,3 +50,24 @@ function formSubmitHandler(event) {
 
   popup.classList.remove('popup_is-opened');
 }
+
+function openPopupCard(event) {
+  event.preventDefault();
+  popup_card.classList.add('popup_is-opened');
+}
+
+function closePopupCard(event) {
+  event.preventDefault();
+  popup_card.classList.remove('popup_is-opened');
+}
+
+function addNewCard(event) {
+  event.preventDefault();
+
+  placeName.textContent = placeInput.value;
+  placeImage.textContent = imageInput.value;
+
+  popup_card.classList.remove('popup_is-opened');
+}
+
+addNewCardButton.addEventListener('click', addNewCard);
