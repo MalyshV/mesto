055049c4ddb83fe7a700f4c21d0e237b7fe4d0aa
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-05-01 23:24:47 +0300

    feat: start likeButton function

--- scripts/index.js
+++ scripts/index.js
@@ -63,8 +63,20 @@ function addNewCard(event) {
   popup_card.classList.remove('popup_is-opened');
 }
 
+// удаление карточки:
+
 const cardRemoveButton = document.querySelector('.element__delete-button');
 
 cardRemoveButton.addEventListener('click', function(e) {
   e.target.closest('.element').remove();
-})
+});
+
+/* лайк карточки: - ДОРАБОТАТЬ
+
+const likeButton = document.querySelector('.element__like-button');
+
+function likeCard(event) {
+  event.target.classList.toggle('.element__like-button_active');
+}
+
+likeButton.addEventListener('click', likeCard)*/
