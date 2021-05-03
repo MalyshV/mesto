f6d83b3ff480ca5b1b3ec5369d9d63ce75b1e17c
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-05-03 17:32:22 +0300

    feat: js for cards

--- scripts/index.js
+++ scripts/index.js
@@ -5,22 +5,41 @@ const closePopupButton = document.querySelector('.popup__close');
 const formElement = document.querySelector('[name="profile-form"]');
 const openPopupCardButton = document.querySelector('.profile__add-button');
 const closePopupCardButton = document.querySelector('#button');
-const addNewCardButton = document.querySelector('.input-container__addbutton');
+const addCardButton = document.querySelector('.input-container__addbutton');
+const elementsList = document.querySelector('.elements__list');
 let nameInput = document.querySelector('[name="user-name"]');
 let jobInput = document.querySelector('[name="user-job"]');
 let profileName = document.querySelector('.profile__user-name');
 let profileJob = document.querySelector('.profile__user-job');
-let placeInput = document.querySelector('[name="name"]');
-let imageInput = document.querySelector('[name="link"]');
-let placeName = document.querySelector('.element__title');
-let placeImage = document.querySelector('.element__image');
+// let placeInput = document.querySelector('[name="name"]');
+// let imageInput = document.querySelector('[name="link"]');
+// let placeName = document.querySelector('.element__title');
+// let placeImage = document.querySelector('.element__image');
+
+/*const createCardButton = document.querySelector('.input-container__addbutton');
+//const input = document.querySelector('.input-container');
+const cardContainer = document.querySelector('.elements__list');
+const cardTemplate = document.querySelector('#card-template');
+
+createCardButton.addEventListener('click', function() {
+  const cardValue = input.value;
+  const newCard = cardTemplate.content.querySelector('.element').cloneNode(true);
+
+  const cardName = newCard.querySelector('.element__title');
+  cardName.textContent = cardValue;
+
+  cardContainer.append(newCard);
+});
+*/
+
 
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 closePopupCardButton.addEventListener('click', closePopupCard);
 formElement.addEventListener('submit', formSubmitHandler);
 openPopupCardButton.addEventListener('click', openPopupCard);
-addNewCardButton.addEventListener('click', addNewCard);
+//addCardButton.addEventListener('click', addCard);
+
 
 function openPopup(event) {
   event.preventDefault();
@@ -54,14 +73,19 @@ function closePopupCard(event) {
   popup_card.classList.remove('popup_is-opened');
 }
 
-function addNewCard(event) {
+// Добавление карты
+
+/*function addCard(event, placeName, placeImage) {
   event.preventDefault();
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
-  placeName.textContent = placeInput.value;
-  placeImage.textContent = imageInput.value;
+  cardElement.querySelector('.element__title').textContent = placeName;
+  cardElement.querySelector('.element__image').src = placeImage;
 
-  popup_card.classList.remove('popup_is-opened');
-}
+  elementsList.append(cardElement);
+  popup_card.classList.add('popup_is-opened');
+}*/
 
 // удаление карточки:
 
@@ -73,10 +97,60 @@ cardRemoveButton.addEventListener('click', function(e) {
 
 /* лайк карточки: - ДОРАБОТАТЬ
 
+Урок 8: объект event: Внутри функции addsong
+
+ songElement.querySelector('.song__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('song__like_active');
+  });
+
 const likeButton = document.querySelector('.element__like-button');
 
 function likeCard(event) {
-  event.target.classList.toggle('.element__like-button_active');
+  event.target.classList.toggle('element__like-button_active');
 }
 
-likeButton.addEventListener('click', likeCard)*/
+likeButton.addEventListener('click', likeCard);*/
+
+// Вывод карточек на страницу
+
+const initialCards = [
+  {
+    name: 'Карачаево-Черкесия',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const initialCardsList = document.querySelector('.elements__list');
+const initialCardTemplate = document.querySelector('#card-template').content;
+
+initialCards.forEach(function (element) {
+  const initialCardElement = initialCardTemplate.cloneNode(true);
+
+  initialCardElement.querySelector('.element__title').textContent = element.name;
+
+  initialCardElement.querySelector('.element__image').src = element.link;
+
+  initialCardsList.append(initialCardElement)
+});
+
+
